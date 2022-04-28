2e07d4ccb90679f065da6773255bd8d70d7ed3a0
xuanloc <vanxuanloc@gmail.com>
2022-04-28 16:43:08 +0700

    test

--- src/App.js
+++ src/App.js
@@ -15,19 +15,11 @@ function App() {
             maxlength="30" />
 
       <h1 className="title">This is Tao Page</h1>
-      <h2 className="title">Login Account</h2>
+      <h2 className="title">Login Ac</h2>
       <a href="https://www.petrolimex.com.vn/ndi/thong-cao-bao-chi/petrolimex-dieu-chinh-gia-xang-dau-tu-15-gio-00-ngay-21-4-2022.html">
         Đừng bấm vào
       </a>
-      <form className="form">
-        <p>
-          Username:
-          <input type="text" name="username" size="15" maxlength="30" />
-        </p>
-        <p>
-          Password:
-          <input type="password" name="password" size="15" maxlength="30" />
-        </p>
+      </p>
       </form>
       <button
         style={{ textAlight: "center" }}
