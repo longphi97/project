4701409ea03c79184a7069fa01a5d63461cdc2cf
nguyenanhtuan0799 <nguyenanhtuan0799@gmail.com>
2022-04-28 14:56:15 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const handleSubmit = (e) => {
+    alert("hello");
+  };
   return (
     <div className="App">
-    <h1 className="title">This is LongPhiLong Page</h1>
-    <h2 className='title'>Login Account</h2>
-      <form className='form'>
-        <p>Username:
-          <input type="text" name="username" size="15" 
-            maxlength="30" />
+      <h1 className="title">This is LongPhiLong Page</h1>
+      <h2 className="title">Login Account</h2>
+      <form className="form">
+        <p>
+          Username:
+          <input type="text" name="username" size="15" maxlength="30" />
         </p>
-        <p>Password:
-          <input type="password" name="password" size="15"
-            maxlength="30" />
+        <p>
+          Password:
+          <input type="password" name="password" size="15" maxlength="30" />
         </p>
       </form>
-      <button className="btn">Register</button>
+      <button
+        style={{ textAlight: "center" }}
+        className="btn"
+        type="submit"
+        onClick={handleSubmit}
+      >
+        Register
+      </button>
     </div>
   );
 }
