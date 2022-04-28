aa386c8f3953db87155fbaa2591a97c0ff01cf86
LongPhiLong <gangsteridol977@gmail.com>
2022-04-28 14:35:36 +0700

    long

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <h1 className="title">This is LongPhiLong Page</h1>
+    <h2 className='title'>Login Account</h2>
+      <form className='form'>
+        <p>Username:
+          <input type="text" name="username" size="15" 
+            maxlength="30" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Password:
+          <input type="password" name="password" size="15"
+            maxlength="30" />
+        </p>
+      </form>
+      <button className="btn">Register</button>
     </div>
   );
 }
